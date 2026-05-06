6fae302b051efcb68f1841079322cb58e4b8a667
DJC <yinb9933@gmail.com>
2026-05-06 21:23:23 +0800

    Load non-default locale resources on demand

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,33 +1,73 @@
 import i18n from 'i18next'
+import type { BackendModule, ResourceKey } from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-import enCommon from '../locales/en/common'
-import enExercises from '../locales/en/exercises'
-import enMuscles from '../locales/en/muscles'
 import zhCNCommon from '../locales/zh-CN/common'
 import zhCNExercises from '../locales/zh-CN/exercises'
 import zhCNMuscles from '../locales/zh-CN/muscles'
-import { defaultLanguage, resolveLanguagePreference } from './languages'
+import {
+  defaultLanguage,
+  isSupportedLanguage,
+  resolveLanguagePreference,
+  supportedLanguages,
+  type SupportedLanguage,
+} from './languages'
 import { getSafeInitialLanguagePreference } from './storage'
 
 const initialLanguage = resolveLanguagePreference(getSafeInitialLanguagePreference())
+const localeNamespaces = ['common', 'exercises', 'muscles'] as const
 
-void i18n.use(initReactI18next).init({
+type LocaleNamespace = (typeof localeNamespaces)[number]
+
+const bundledResources = {
+  [defaultLanguage]: {
+    common: zhCNCommon,
+    exercises: zhCNExercises,
+    muscles: zhCNMuscles,
+  },
+}
+
+const localeResourceLoaders = {
+  'zh-CN': {
+    common: () => Promise.resolve(zhCNCommon),
+    exercises: () => Promise.resolve(zhCNExercises),
+    muscles: () => Promise.resolve(zhCNMuscles),
+  },
+  en: {
+    common: () => import('../locales/en/common').then((module) => module.default),
+    exercises: () => import('../locales/en/exercises').then((module) => module.default),
+    muscles: () => import('../locales/en/muscles').then((module) => module.default),
+  },
+} satisfies Record<SupportedLanguage, Record<LocaleNamespace, () => Promise<ResourceKey>>>
+
+function isLocaleNamespace(value: string): value is LocaleNamespace {
+  return localeNamespaces.includes(value as LocaleNamespace)
+}
+
+const dynamicLocaleBackend: BackendModule = {
+  type: 'backend',
+  init() {},
+  read(language, namespace, callback) {
+    if (!isSupportedLanguage(language) || !isLocaleNamespace(namespace)) {
+      callback(new Error(`Unsupported locale resource: ${language}/${namespace}`), false)
+      return
+    }
+
+    void localeResourceLoaders[language][namespace]()
+      .then((resource) => callback(null, resource))
+      .catch((error: unknown) => {
+        callback(error instanceof Error ? error : new Error(String(error)), false)
+      })
+  },
+}
+
+export const i18nReady = i18n.use(dynamicLocaleBackend).use(initReactI18next).init({
   lng: initialLanguage,
   fallbackLng: defaultLanguage,
-  supportedLngs: ['zh-CN', 'en'],
-  resources: {
-    'zh-CN': {
-      common: zhCNCommon,
-      exercises: zhCNExercises,
-      muscles: zhCNMuscles,
-    },
-    en: {
-      common: enCommon,
-      exercises: enExercises,
-      muscles: enMuscles,
-    },
-  },
+  supportedLngs: [...supportedLanguages],
+  ns: [...localeNamespaces],
+  resources: bundledResources,
+  partialBundledLanguages: true,
   defaultNS: 'common',
   interpolation: {
     escapeValue: false,
@@ -39,7 +79,8 @@ function syncDocumentLanguage(language: string) {
   document.title = i18n.t('common.appName')
 }
 
-syncDocumentLanguage(initialLanguage)
 i18n.on('languageChanged', syncDocumentLanguage)
+
+void i18nReady.then(() => syncDocumentLanguage(i18n.resolvedLanguage ?? initialLanguage))
 
 export default i18n
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
 import './app/pwa.ts'
-import './i18n'
+import { i18nReady } from './i18n'
 import './styles/global.css'
 import { applyStoredThemePreference } from './lib/theme.ts'
 
 applyStoredThemePreference()
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+void i18nReady.then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
